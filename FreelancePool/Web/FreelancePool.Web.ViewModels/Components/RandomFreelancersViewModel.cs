b1a9e09dea5ea474b0b5f044604f13e728a5dc82
MinaMarinova <mina.m.marinova@gmail.com>
2020-04-11 00:50:15 +0300

    Create action and viewModel for profile details.

--- FreelancePool/Web/FreelancePool.Web.ViewModels/Components/RandomFreelancersViewModel.cs
+++ FreelancePool/Web/FreelancePool.Web.ViewModels/Components/RandomFreelancersViewModel.cs
@@ -14,6 +14,8 @@
             this.Categories = new HashSet<string>();
         }
 
+        public string Id { get; set; }
+
         public string UserName { get; set; }
 
         public string PhotoUrl { get; set; }
